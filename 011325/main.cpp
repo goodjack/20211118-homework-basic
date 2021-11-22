2a5bc8c718c34d3039fc0016b1905181eeb8ce59
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-23 01:02:58 +0800

    方便看

--- 011325/main.cpp
+++ 011325/main.cpp
@@ -6,8 +6,9 @@ int main() {
     cout << "高度: ";
     cin >> h;
 
-    for (int i = 1; i <= h; i++) {
-        for (int j = h; j > 0; j--) {
+    for (int i = 1; i <= h; i++) {//直行
+
+        for (int j = h; j > 0; j--) {//橫排
             if (i < j) {
                 cout << " ";
             } else {
@@ -15,9 +16,11 @@ int main() {
             }
         }
         cout << " ";
+
         for (int j = 0; j < i; j++) {
             cout << "#";
         }
+        
         cout << endl;
     }
 } 
