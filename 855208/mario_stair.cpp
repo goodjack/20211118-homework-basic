f18acd6afe93e06dced391b18889bf9478edba61
Chaoray <65358638+Chaoray@users.noreply.github.com>
2021-11-22 08:49:11 +0800

    改善排版

--- 855208/mario_stair.cpp
+++ 855208/mario_stair.cpp
@@ -2,17 +2,15 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
+int main() {
 	int a = 0;
   printf("What's your stair's height: ");
   scanf("%d", &a);
 
-  for (int i = 0; i < a + 1; i++) 
- {
-    for(int n = 0; n < a - i; n++) 
- {
+  for (int i = 0; i < a + 1; i++) {
+    for(int n = 0; n < a - i; n++) {
         printf(" ");
-      }
+    }
     
     for (int z = 0; z < 2; z++) {
       for(int n = 0; n < i; n++) {
